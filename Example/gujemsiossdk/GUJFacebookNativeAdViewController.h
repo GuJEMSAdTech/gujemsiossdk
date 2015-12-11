5ac0437d28f2c76eb5f72903c7be302b83f07e0a
Michael Brügmann <mail@michael-bruegmann.de>
2015-12-11 16:13:45 +0100

    example for FB Native Ads

--- Example/gujemsiossdk/GUJFacebookNativeAdViewController.h
+++ Example/gujemsiossdk/GUJFacebookNativeAdViewController.h
@@ -4,7 +4,24 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "FBNativeAd.h"
+#import <FBAudienceNetwork/FBAudienceNetwork.h>
 
 
-@interface GUJFacebookNativeAdViewController : NSObject
+@interface GUJFacebookNativeAdViewController : UIViewController <FBNativeAdDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *placementIdTextField;
+@property (weak, nonatomic) IBOutlet UILabel *errorLabel;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
+@property (weak, nonatomic) IBOutlet UIImageView *adIconImageView;
+@property (weak, nonatomic) IBOutlet UILabel *adTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *adBodyLabel;
+@property (weak, nonatomic) IBOutlet UIButton *adCallToActionButton;
+@property (weak, nonatomic) IBOutlet UILabel *adSocialContextLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sponsoredLabel;
+
+@property (strong , nonatomic) FBMediaView *adCoverMediaView;
+
+@property (weak, nonatomic) IBOutlet UIView *adView;
+
 @end
--- Example/gujemsiossdk/GUJFacebookNativeAdViewController.m
+++ Example/gujemsiossdk/GUJFacebookNativeAdViewController.m
@@ -7,6 +7,55 @@
 
 
 @implementation GUJFacebookNativeAdViewController {
-
 }
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    self.activityIndicator.hidesWhenStopped = YES;
+    self.placementIdTextField.text = @"1573876952861381_1640386426210433";
+}
+
+- (void)nativeAdDidLoad:(FBNativeAd *)nativeAd {
+    [self.activityIndicator stopAnimating];
+
+    [self.adTitleLabel setText:nativeAd.title];
+    [self.adBodyLabel setText:nativeAd.body];
+    [self.adSocialContextLabel setText:nativeAd.socialContext];
+    [self.sponsoredLabel setText:@"Sponsored"];
+    [self.adCallToActionButton setTitle:nativeAd.callToAction forState:UIControlStateNormal];
+
+    [nativeAd.icon loadImageAsyncWithBlock:^(UIImage *image) {
+        [self.adIconImageView setImage:image];
+    }];
+
+    // Allocate a FBMediaView to contain the cover image or native video asset
+    self.adCoverMediaView = [[FBMediaView alloc] initWithFrame:CGRectMake(0, 0, 320, 400)];
+    [self.adCoverMediaView setNativeAd:nativeAd];
+
+    // Add adChoicesView
+    FBAdChoicesView *adChoicesView = [[FBAdChoicesView alloc] initWithNativeAd:nativeAd];
+    [self.adView addSubview:adChoicesView];
+    [adChoicesView updateFrameFromSuperview];
+
+    // Register the native ad view and its view controller with the native ad instance
+    [nativeAd registerViewForInteraction:self.adView withViewController:self];
+}
+
+- (void)nativeAd:(FBNativeAd *)nativeAd didFailWithError:(NSError *)error {
+    [self.activityIndicator stopAnimating];
+    self.errorLabel.text = error.localizedDescription;
+}
+
+- (IBAction)loadFacebookNativeAd:(id)sender {
+    [self.activityIndicator startAnimating];
+    self.errorLabel.text = @"";
+
+    FBNativeAd *nativeAd =
+    [[FBNativeAd alloc] initWithPlacementID:self.placementIdTextField.text];
+    nativeAd.delegate = self;
+    [nativeAd loadAd];
+}
+
 @end
