9ed19461eba3c26d6d9f346cf95cfcb7b558465e
Michael Brügmann <mail@michael-bruegmann.de>
2016-02-17 15:48:22 +0100

    updated copyright of new classes

--- Pod/Classes/GUJInflowAdViewContext.m
+++ Pod/Classes/GUJInflowAdViewContext.m
@@ -1,6 +1,6 @@
 /*
  * BSD LICENSE
- * Copyright (c) 2015, Mobile Unit of G+J Electronic Media Sales GmbH, Hamburg All rights reserved.
+ * Copyright (c) 2016, Mobile Unit of G+J Electronic Media Sales GmbH, Hamburg All rights reserved.
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
  * Redistributions of source code must retain the above copyright notice,
