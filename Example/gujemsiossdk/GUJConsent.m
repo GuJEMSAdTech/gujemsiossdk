c0a45cc45981b18a7dfb70e43e1c0cdcee348acf
Gohl <gohl2@gujnm271270.global.intra.guj.com>
2019-01-23 17:24:16 +0100

    Yieldlab and Emetriq #2

--- Example/gujemsiossdk/GUJConsent.m
+++
@@ -1,9 +0,0 @@
-//
-//  GUJConsent.m
-//  gujemsiossdk_Example
-//
-//  Created by Gohl, Michael on 21.01.19.
-//  Copyright © 2019 Michael Brügmann. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
